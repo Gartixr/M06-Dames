b5a10557e59fc04d1ad4e7bf957f9b74d1f6a78a
Gartixr <adidumitrica@gmail.com>
2021-01-29 18:21:34 +0100

    Metodo mostrar menu

--- M06-Dames/src/App.java
+++ M06-Dames/src/App.java
@@ -1,11 +1,35 @@
+import java.util.Scanner;
 
 public class App {
 
+	static Scanner teclado = new Scanner(System.in);
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Test");
+		boolean jugar = true;
 		
+		cargarMenu();
+		
+		int input = teclado.nextInt();
+		
+		if(input == 1) {
+			
+		} else if (input == 2) {
+			
+		} else {
+			
+		}
+		
+	}
+
+	private static void cargarMenu() {
+		// TODO Auto-generated method stub
+		
+		System.out.println("[1] - Nueva partida \n"
+				+ "[2] - Cargar partida \n"
+				+ "[3] - Salir");
+
 	}
 
 }
